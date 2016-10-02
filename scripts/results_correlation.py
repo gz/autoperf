c8c474b04b7e0d753fb73f8163d67207650ec6a3
Gerd Zellweger <mail@gerdzellweger.com>
2016-10-02 13:44:26 +0200

    More consistent naming scheme.
    Signed-off-by: Gerd Zellweger <mail@gerdzellweger.com>

---
+++ scripts/results_correlation.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import pandas as pd
+import numpy as np
+
+from util import *
+
+
+def persist_correlation(correlation_file, events, correlation_matrix):
+    "Writes the correlation matrix (pairwise correlation value for every event) to file."
+    with open(correlation_file, 'w') as f:
+        header = '\t{}\n'.format('\t'.join([str(i) for i in events]))
+        f.write(header)
+        for i in events:
+            f.write('{}'.format(i))
+            for j in events:
+                f.write('\t{}'.format(correlation_matrix.ix[i, j]))
+            f.write('\n')
+
+def persist_correlated_events(correlated_events_file, events, event_names, correlation_matrix):
+    with open(correlated_events_file, 'w') as f:
+        degree_to_events_dict = {}
+        # Find correlated events for each event
+        for i in events:
+            correlated_events = [
+                j for j in events
+                if i != j and correlation_matrix.ix[i, j] >= CORRELATION_CUTOFF
+            ]
+            n = len(correlated_events)
+            if n not in degree_to_events_dict:
+                degree_to_events_dict[n] = []
+            degree_to_events_dict[n].append(i)
+
+            f.write('Event {} {} ({})\n'.format(i, event_names[i],
+                                                len(correlated_events)))
+            for j in correlated_events:
+                f.write('\t{:3d} {:.2f} {}\n'.format(
+                    j, correlation_matrix.ix[i, j], event_names[j])
+                )
+        f.write('-' * 50 + '\n')
+        for n in sorted(degree_to_events_dict, reverse=True):
+            l = degree_to_events_dict[n]
+            f.write('{} ({}): {}\n'.format(n, len(l),
+                                           ', '.join([str(i) for i in l])))
+
+def persist_excluded_events(excluded_events_file, excluded_events):
+    with open(excluded_events_file, 'w') as f:
+        # Find correlated events for each event
+        for i in excluded_events:
+            f.write('{}\n'.format(i))
+
+def correlation_matrix(data_directory):
+    df = load_as_X(os.path.join(data_directory, 'result.csv'))
+
+    correlation_matrix = df.corr()
+    # Ensure all values in correlation matrix are valid
+    for i in events:
+        for j in events:
+            assert not np.isnan(correlation_matrix.ix[i, j])
+
+    correlation_file = os.path.join(data_directory, 'correlation_matrix.csv')
+    correlation_matrix.to_csv(correlation_file)
+
+    # Write correlation matrix
+    correlation_file = os.path.join(data_directory, 'event_correlation.csv')
+    correlation_matrix.to_csv(correlation_file)
+    sys.exit(1)
+    persist_correlation(correlation_file, events, correlation_matrix)
+
+    # Get event names
+    """
+    events_file = os.path.join(data_directory, 'events.dat')
+    event_names = pd.read_csv(events_file, sep='\t', header=None, index_col=0)
+    event_names = event_names.to_dict()[1]
+
+    # Write correlated events for each event
+    correlated_events_file = os.path.join(
+        plot_output_dir,
+        'correlated_events.{}.dat'.format(int(CORRELATION_CUTOFF * 100))
+    )
+    persist_correlated_events(correlated_events_file, events, event_names,
+                              correlation_matrix)
+
+    # Write excluded events
+    excluded_events_file = os.path.join(plot_output_dir,
+                                        'excluded_events.dat')
+    persist_excluded_events(excluded_events_file, excluded_events)
+    """
+
+
+def usage(progname):
+    print >> sys.stderr, 'usage:', progname, '[data_input_dir]'
+    sys.exit(0)
+
+if __name__ == '__main__':
+    if len(sys.argv) > 2:
+        usage(sys.argv[0])
+    correlation_matrix(sys.argv[1])
--- scripts/util.py
+++ scripts/util.py
@@ -1,5 +1,81 @@
 import pandas as pd
 
+def load_as_X(f, aggregate_samples=True, remove_zero=True, cut_off_nan=True):
+    """
+    Transform CSV file into a matrix X (used for most ML inputs).
+    The rows will be different times, the columns are the events.
+
+    Keyword arguments:
+    aggregate_samples -- Aggregate samples from all CPUs at time t.
+    remove_zero -- Remove features that are all zero.
+    cut_off_nan -- Remove everything after first NaN value is encountered.
+    """
+    # Parse file
+    raw_data = pd.read_csv(f, sep=',', skipinitialspace=True)
+    raw_data.sortlevel(inplace=True)
+
+    # Convert time
+    time_to_ms(raw_data)
+
+    # Aggregate all event samples from the same event at time
+    if aggregate_samples:
+        grouped_df = raw_data.groupby(['EVENT_NAME', 'TIME']).sum()
+        grouped_df.reset_index(level=['TIME'], inplace=True)
+
+    # Remove events whose deltas are all 0:
+    if remove_zero:
+        df = grouped_df.drop(get_all_zero_events(grouped_df))
+        df = result_to_matrix(df)
+
+    # Cut off everything after first NaN value:
+    if cut_off_nan:
+        cut_off = minimum_nan_index(df)
+        if cut_off != None:
+            df = df[:cut_off]
+
+    return df
+
+def result_to_matrix(df):
+    """
+    Transform the result as read from results.csv in a matrix of the following format:
+
+    EVENT1  EVENT2  EVENT3 .... EVENTN
+    12           9       5          12
+     1           1       2           5
+     0         NaN     100          12
+     0         NaN     NaN          99
+
+    Note: NaN numbers may appear for an event at the end in case the individual events
+    can be read from different runs containing a different amount of samples.
+    Differences of just a few samples is normally not a problem. Big discrepancies
+    would indicate unstable runtimes of your algorithm.
+    """
+    frames = []
+    for idx in df.index.unique():
+        series = df.loc[[idx], 'SAMPLE_VALUE']
+        new_series = series.rename(idx).reset_index(drop=True)
+        frames.append(new_series)
+
+    # Column i is event i
+    return pd.concat(frames, axis=1)
+
+def minimum_nan_index(df):
+    """
+    Return the earliest index that contains NaN over all columns or None
+    if there are no NaN values in any columns.
+
+    # Example
+    For the following matrix it returns 1 as (1,1) is NaN:
+    idx | EVENT1   EVENT2  EVENT3 .... EVENTN
+      0 |     12        9       5          12
+      1 |      1      NaN       2           5
+      2 |      0      NaN     100          12
+      3 |      0      NaN       1          99
+    """
+    for idx, has_null in df.isnull().any(axis=1).iteritems():
+        if has_null:
+            return idx
+
 def get_all_zero_events(df):
     """
     Given a pandas DataFrame loaded from a results.csv file,
@@ -10,3 +86,10 @@ def get_all_zero_events(df):
         if df.loc[idx, 'SAMPLE_VALUE'].sum() == 0:
             event_names.append(idx)
     return event_names
+
+def time_to_ms(df):
+    """
+    Transforn the perf time (floating point, seconds)
+    to miliseconds (absolute numbers)
+    """
+    df['TIME'] = df['TIME'].map(lambda x: int(x * 1000))
