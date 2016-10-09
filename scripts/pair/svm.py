e0f8c7a6ff0979065a354d5f5b65a6d91c45e630
Gerd Zellweger <mail@gerdzellweger.com>
2016-10-10 00:34:44 +0200

    Update SVM script.
    Signed-off-by: Gerd Zellweger <mail@gerdzellweger.com>

--- scripts/pair/svm.py
+++ scripts/pair/svm.py
@@ -11,31 +11,24 @@ import numpy as np
 from runtimes import get_runtime_dataframe, get_runtime_pivot_tables
 from util import *
 
-def add_to_classifier(X, Y):
-    X = None
-    Y = None
-    return (X, Y)
+from sklearn import svm
+from sklearn.model_selection import cross_val_score
 
 def make_matrix(results_file, output_file):
     df = load_as_X(results_file, aggregate_samples='meanstd', cut_off_nan=True)
     df.to_csv(output_file, index=False)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Generate some CSV files for weka SVM.')
-    parser.add_argument('data_directory', type=str, help="Data directory root")
-    parser.add_argument('--config', dest='config', nargs='+', type=str, help="Which configs to include (L3-SMT, L3-SMT-cores, ...)")
-    parser.add_argument('--test', dest='test', nargs='+', type=str, help="Which programs to use as the test set")
-    parser.add_argument('--uncore', dest='uncore', nargs='+', type=str, help="What uncore counters to include.", default='shared')
-
-    args = parser.parse_args()
-    print(args.data_directory)
-    print(args.config)
-    print(args.test)
-    print(args.uncore)
-
     pd.set_option('display.max_rows', 10)
     pd.set_option('display.max_columns', 5)
     pd.set_option('display.width', 160)
+
+    parser = argparse.ArgumentParser(description='Get the SVM accuracy for all programs.')
+    parser.add_argument('data_directory', type=str, help="Data directory root.")
+    parser.add_argument('--config', dest='config', nargs='+', type=str, help="Which configs to include (L3-SMT, L3-SMT-cores, ...).")
+    parser.add_argument('--uncore', dest='uncore', nargs='+', type=str, help="What uncore counters to include.", default='shared')
+    parser.add_argument('--test', dest='test', nargs='+', type=str, help="Which programs to use as the test set.")
+    args = parser.parse_args()
 
     ## Settings:
     MATRIX_FILE = 'matrix_X_uncore_{}.csv'.format(args.uncore)
@@ -55,7 +48,7 @@ if __name__ == '__main__':
                 for (i, normalized_runtime) in enumerate(values):
                     B = table.columns[i]
 
-                    classification = 'T' if normalized_runtime > CLASSIFIER_CUTOFF else 'F'
+                    classification = 1 if normalized_runtime > CLASSIFIER_CUTOFF else 0
                     results_path = os.path.join(args.data_directory, config, "{}_vs_{}".format(A, B))
                     matrix_file = os.path.join(results_path, MATRIX_FILE)
 
@@ -78,10 +71,17 @@ if __name__ == '__main__':
 
     training_file_name = 'svm_training_without_{}_{}_uncore_{}.csv'.format('_'.join(args.test), '_'.join(args.config), args.uncore)
     X['Y'] = Y
-    print X
+    #print X
     X.to_csv(os.path.join(args.data_directory, training_file_name), index=False)
 
     test_file_name = 'svm_test_{}_{}_uncore_{}.csv'.format('_'.join(args.test), '_'.join(args.config), args.uncore)
     X_test['Y'] = Y_test
-    print X_test
+    #print X_test
     X_test.to_csv(os.path.join(args.data_directory, test_file_name), index=False)
+
+    clf = svm.SVC()
+    clf.fit(X.as_matrix(), Y.as_matrix())
+    print "Test {}".format(args.test), clf.score(X_test, Y_test)
+
+    #model = svm.SVC()
+    #print cross_val_score(model, X_test, Y_test, scoring='accuracy')
