714827f38c3de3d0a66c60d104bf3b7d987fc3e8
Gerd Zellweger <mail@gerdzellweger.com>
2016-10-09 23:48:29 +0200

    Update svm script.
    Signed-off-by: Gerd Zellweger <mail@gerdzellweger.com>

--- scripts/pair/svm.py
+++ scripts/pair/svm.py
@@ -3,6 +3,7 @@
 import os
 import sys
 import time
+import argparse
 
 import pandas as pd
 import numpy as np
@@ -20,28 +21,42 @@ def make_matrix(results_file, output_file):
     df.to_csv(output_file, index=False)
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Generate some CSV files for weka SVM.')
+    parser.add_argument('data_directory', type=str, help="Data directory root")
+    parser.add_argument('--config', dest='config', nargs='+', type=str, help="Which configs to include (L3-SMT, L3-SMT-cores, ...)")
+    parser.add_argument('--test', dest='test', nargs='+', type=str, help="Which programs to use as the test set")
+    parser.add_argument('--uncore', dest='uncore', nargs='+', type=str, help="What uncore counters to include.", default='shared')
+
+    args = parser.parse_args()
+    print(args.data_directory)
+    print(args.config)
+    print(args.test)
+    print(args.uncore)
+
     pd.set_option('display.max_rows', 10)
     pd.set_option('display.max_columns', 5)
     pd.set_option('display.width', 160)
 
     ## Settings:
-    MATRIX_FILE = 'matrix_X_uncore_shared.csv'
-    TO_BUILD = ['L3-SMT'] # 'L3-SMT-cores'
+    MATRIX_FILE = 'matrix_X_uncore_{}.csv'.format(args.uncore)
     CLASSIFIER_CUTOFF = 1.15
+
+    X_test = pd.DataFrame()
+    Y_test = pd.Series()
 
     X = pd.DataFrame()
     Y = pd.Series()
 
-    runtimes = get_runtime_dataframe(sys.argv[1])
+    runtimes = get_runtime_dataframe(args.data_directory)
 
     for config, table in get_runtime_pivot_tables(runtimes):
-        if config in TO_BUILD:
+        if config in args.config:
             for (A, values) in table.iterrows():
                 for (i, normalized_runtime) in enumerate(values):
                     B = table.columns[i]
 
-                    classification = 'Y' if normalized_runtime > CLASSIFIER_CUTOFF else 'N'
-                    results_path = os.path.join(sys.argv[1], config, "{}_vs_{}".format(A, B))
+                    classification = 'T' if normalized_runtime > CLASSIFIER_CUTOFF else 'F'
+                    results_path = os.path.join(args.data_directory, config, "{}_vs_{}".format(A, B))
                     matrix_file = os.path.join(results_path, MATRIX_FILE)
 
                     if os.path.exists(os.path.join(results_path, 'completed')):
@@ -50,11 +65,23 @@ if __name__ == '__main__':
                             sys.exit(1)
                         df = pd.read_csv(matrix_file, index_col=False)
 
-                        Y = pd.concat([Y, pd.Series([classification for _ in range(0, df.shape[0])])])
-                        X = pd.concat([X, df])
+                        if A in args.test or B in args.test:
+                            #print "Adding {} vs {} to test set".format(A, B)
+                            Y_test = pd.concat([Y_test, pd.Series([classification for _ in range(0, df.shape[0])])])
+                            X_test = pd.concat([X_test, df])
+                        else:
+                            Y = pd.concat([Y, pd.Series([classification for _ in range(0, df.shape[0])])])
+                            X = pd.concat([X, df])
                     else:
                         print "Exclude unfinished directory {}".format(results_path)
 
+
+    training_file_name = 'svm_training_without_{}_{}_uncore_{}.csv'.format('_'.join(args.test), '_'.join(args.config), args.uncore)
     X['Y'] = Y
-    print X.shape
-    X.to_csv(os.path.join(sys.argv[1], 'wekka_xy_L3-SMT_uncore_shared.csv'), index=False)
+    print X
+    X.to_csv(os.path.join(args.data_directory, training_file_name), index=False)
+
+    test_file_name = 'svm_test_{}_{}_uncore_{}.csv'.format('_'.join(args.test), '_'.join(args.config), args.uncore)
+    X_test['Y'] = Y_test
+    print X_test
+    X_test.to_csv(os.path.join(args.data_directory, test_file_name), index=False)
