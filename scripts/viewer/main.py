56b5332d57f8d0833142a390fc9cf0f034d0af21
Gerd Zellweger <mail@gerdzellweger.com>
2016-10-21 23:49:22 +0200

    Update plot with Max/Min.
    Signed-off-by: Gerd Zellweger <mail@gerdzellweger.com>

--- scripts/viewer/main.py
+++ scripts/viewer/main.py
@@ -9,32 +9,20 @@ from bokeh.io import curdoc
 from bokeh.layouts import gridplot, column, row
 from bokeh.charts import Line, show, output_file
 from bokeh.models.widgets import PreText, Select
-from bokeh.models import ColumnDataSource
+from bokeh.models import ColumnDataSource, Range1d
 from bokeh.plotting import figure
+from bokeh.palettes import Spectral11, Blues4
 
 from flask import request
 
-config = curdoc().session_context.request.arguments['config'][0]
-folder = curdoc().session_context.request.arguments['folder'][0]
+if curdoc().session_context.request.arguments.has_key('config'):
+    config = curdoc().session_context.request.arguments['config'][0]
+if curdoc().session_context.request.arguments.has_key('folder'):
+    folder = curdoc().session_context.request.arguments['folder'][0]
 
-RESULTS_BASE = '/home/zgerd/workspace/results-babybel/'
+RESULTS_BASE = '/home/gz/workspace/results-babybel/'
 
 df_csv = pd.read_csv(os.path.join(RESULTS_BASE, config, folder, "results_uncore_shared.csv"))
-df = pd.read_csv(os.path.join(RESULTS_BASE, config, folder, "matrix_X_uncore_shared.csv"))
-
-def get_matrix_file(results_path):
-    MATRIX_FILE = 'matrix_X_uncore_{}.csv'.format(args.uncore)
-
-    matrix_file = os.path.join(results_path, MATRIX_FILE)
-    if os.path.exists(os.path.join(results_path, 'completed')):
-        if os.path.exists(matrix_file):
-            return matrix_file
-        else:
-            print "No matrix file ({}) found, run the scripts/pair/matrix_all.py script first!".format(matrix_file)
-            sys.exit(1)
-    else:
-        print "Unfinished directory: {}".format(results_path)
-        sys.exit(1)
 
 def get_menu():
     dirs = {'L3-SMT': [], 'L3-SMT-cores': []}
@@ -49,60 +37,55 @@ def get_menu():
     return dirs
 
 def get_events(df):
-    print df.columns.values
-    events = [ val.split('.', 1)[1] for val in df.columns.values ]
+    events = df['EVENT_NAME'].values
     return sorted(list(set(events)))
 
-def avg_index(event):
-    return "AVG.{}".format(event)
+def get_xs_ys_for(event_name):
+    xs = []
+    ys = []
+    relevant_events =  df_csv[df_csv['EVENT_NAME'] == event_name]
+    relevant_events_by_cpu = relevant_events.set_index('CPU')
+    for idx in relevant_events_by_cpu.index.unique():
+        xs.append(relevant_events_by_cpu.loc[idx]['TIME'])
+        ys.append(relevant_events_by_cpu.loc[idx]['SAMPLE_VALUE'])
+    return xs, ys
 
-def std_index(event):
-    return "STD.{}".format(event)
+def get_series_for(event_name):
+    events = df_csv[df_csv.EVENT_NAME == event_name]
+    minimum = events.groupby('TIME').min()['SAMPLE_VALUE']
+    mean = events.groupby('TIME').mean()['SAMPLE_VALUE']
+    maximum = events.groupby('TIME').max()['SAMPLE_VALUE']
+    ts = mean.index
+    left = mean.index - 0.125
+    right = mean.index + 0.125
+    return ts, mean, maximum, minimum, left, right
 
-all_events = get_events(df)
-
+all_events = get_events(df_csv)
 
 ticker = Select(value=all_events[0], options=all_events, width=500)
 
-source_avg = ColumnDataSource(data=dict(x=df.index.values, y=df[avg_index(all_events[0])]))
-source_std = ColumnDataSource(data=dict(x=df.index.values, y=df[std_index(all_events[0])]))
+x, average, maximum, minimum, left, right = get_series_for(all_events[0])
+avg_source = ColumnDataSource( data=dict(x=x, average=average, max=maximum, min=minimum, left=left, right=right) )
+
+xs, ys = get_xs_ys_for(all_events[0])
+cpus_source = ColumnDataSource(dict(xs=xs, ys=ys, color=Spectral11))
 
 def ticker_change(attrname, old, new):
-    source_avg.data = dict(x=df.index.values, y=df[avg_index(new)])
-    source_std.data = dict(x=df.index.values, y=df[std_index(new)])
+    x, average, maximum, minimum, left, right = get_series_for(new)
+    avg_source.data = dict(x=x, average=average, max=maximum, min=minimum, left=left, right=right)
+    xs, ys = get_xs_ys_for(new)
+    cpus_source.data = dict(xs=xs, ys=ys, color=Spectral11)
 
 ticker.on_change('value', ticker_change)
 
-fig_avg = figure(title='Average', height=300)
-line = fig_avg.line('x', 'y', source=source_avg, line_width=5)
+fig_avg = figure()
+variation = fig_avg.quad(top='max', bottom='min', left='left', right='right', source=avg_source, color=Blues4[1], legend="Max/Min", alpha=0.5)
+line = fig_avg.line(x='x', y='average', source=avg_source, line_width=5, legend="Average", color=Blues4[0])
 
-fig_std = figure(title='Standard Deviation', height=300)
-line = fig_std.line('x', 'y', source=source_std, line_width=5)
+fig_cpus = figure(title='Individual CPUs')
+cpus_plot = fig_cpus.multi_line(xs='xs', ys='ys', source=cpus_source, line_width=3, line_color='color')
 
-
-xs = []
-ys = []
-def get_xs_ys_for(event_name):
-    print df_csv
-    relevant_events =  df_csv[df_csv['EVENT_NAME'] == 'UOPS_EXECUTED.CORE_CYCLES_GE_2']
-
-    relevant_events.set_index('CPU')
-    for idx in relevant_events.index.values:
-        print idx
-        xs.append(relevant_events.loc[idx]['TIME'])
-        ys.append(relevant_events.loc[idx]['SAMPLE_VALUE'])
-    return xs, ys
-
-xs, ys = get_xs_ys_for('UOPS_EXECUTED.CORE_CYCLES_GE_2')
-cpus_source = ColumnDataSource(dict(xs=xs, ys=ys))
-fig_cpus = figure(title='Individual CPUs', height=300)
-cpus_plot = fig_cpus.multi_line(xs='xs', ys='ys', source=cpus_source, line_width=5)
-
-widgets = column(ticker, column(
-                            row(fig_avg, fig_std, sizing_mode='scale_width'),
-                            fig_cpus, sizing_mode='scale_width'), sizing_mode='scale_width')
-
-
+widgets = column(ticker, column(fig_avg, fig_cpus, sizing_mode='scale_width'), sizing_mode='scale_width')
 env = Environment(loader=FileSystemLoader(os.path.join(sys.path[0], "templates")))
 env.globals['folders'] = get_menu()
 template = env.get_template('events.html')
