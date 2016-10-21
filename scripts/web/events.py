2d9df24b2b910fcbf8bab9933f6189d5a5be56b1
Gerd Zellweger <mail@gerdzellweger.com>
2016-10-21 09:54:14 +0200

    Update events.py bokeh script.
    Signed-off-by: Gerd Zellweger <mail@gerdzellweger.com>

--- scripts/web/events.py
+++ scripts/web/events.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import numpy as np
 
+from jinja2 import Template
 from bokeh.io import curdoc
 from bokeh.layouts import gridplot, column, row
-from bokeh.charts import Line, show, output_file, defaults
+from bokeh.charts import Line, show, output_file
 from bokeh.models.widgets import PreText, Select
 from bokeh.models import ColumnDataSource
 from bokeh.plotting import figure, output_file, show
@@ -11,13 +12,8 @@ from bokeh.plotting import figure, output_file, show
 
 df = pd.read_csv('matrix_X_uncore_shared.csv')
 
-ticker = Select(value="XXX", options=list(df.columns.values), width=500)
-
-defaults.plot_width = 450
-defaults.plot_height = 400
-
-
-source = ColumnDataSource(data=dict(x=[], y=[]))
+ticker = Select(value=df.columns.values[0], options=list(df.columns.values), width=500)
+source = ColumnDataSource(data=dict(x=df.index.values, y=df[df.columns.values[0]]))
 
 #print ColumnDataSource.from_df(df['AVG.ARITH.FPU_DIV'])
 #line.source.data = ColumnDataSource.from_df(df['AVG.ARITH.FPU_DIV'])
@@ -29,9 +25,16 @@ def ticker_change(attrname, old, new):
     source.data = dict(x=df.index.values, y=df[new])
 ticker.on_change('value', ticker_change)
 
-fig = figure(title='Event', width=1024, height=768)
+fig = figure(title='Event')
 
 line = fig.line('x', 'y', source=source, line_width=5)
-widgets = column(ticker, fig)
+widgets = column(ticker, fig, sizing_mode='scale_width')
+
+
+# Open our custom template
+with open('templates/events.html', 'r') as f:
+    template = Template(f.read())
+
 curdoc().add_root(widgets)
 curdoc().title = "Events"
+curdoc().template = template
