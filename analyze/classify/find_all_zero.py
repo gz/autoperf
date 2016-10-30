c3d10d84795542534ebe7a63e42a742954158053
Gerd Zellweger <mail@gerdzellweger.com>
2016-10-30 12:24:53 +0100

    Lazy generation of zero features.
    Signed-off-by: Gerd Zellweger <mail@gerdzellweger.com>

--- analyze/classify/find_all_zero.py
+++ analyze/classify/find_all_zero.py
@@ -13,22 +13,31 @@ from analyze.util import get_zero_features_in_matrix
 def mkfilename(prefix, configs, uncore):
     return "{}_{}_uncore_{}.csv".format(prefix, '_'.join(configs), uncore)
 
-def all_zero_features(configs, uncore):
-    feature_filename = mkfilename("zero_features", args.config, args.uncore)
-    df.to_csv(os.path.join(args.data_directory, feature_filename), index=False)
+def calculate_zero_features(data_directory, configs, uncore):
+    X, Y, Y_weights, X_test, Y_test = svm.row_training_and_test_set(data_directory, configs, [None], uncore=uncore, cutoff=1.00, include_alone=False)
+    features = get_zero_features_in_matrix(X)
+    return features
+
+def all_zero_features(data_directory, configs, uncore):
+    feature_filename = mkfilename("zero_features", configs, uncore)
+    zero_features_path = os.path.join(data_directory, feature_filename)
+    event_filename = mkfilename("zero_events", configs, uncore)
+    zero_events_path = os.path.join(data_directory, feature_filename)
+
+    if not os.path.exists(zero_features_path):
+        features = calculate_zero_features(data_directory, configs, uncore)
+        df = pd.DataFrame(features)
+        df.to_csv(zero_features_path, index=False, header=['EVENT_NAME'])
+
+        events = sorted(set([ feature.split(".", 1)[1] for feature in features ]))
+        df = pd.DataFrame(events)
+        df.to_csv(zero_events_path, index=False, header=['EVENT_NAME'])
+
+    assert os.path.exists(zero_features_path)
+    return pd.read_csv(zero_features_path)
 
 if __name__ == '__main__':
     parser = get_argument_parser('Figures out how many events are 0.', arguments=['data', 'config', 'uncore'])
     args = parser.parse_args()
 
-    X, Y, Y_weights, X_test, Y_test = svm.row_training_and_test_set(args.data_directory, args.config, [None], uncore=args.uncore, cutoff=1.00, include_alone=False)
-
-    features = get_zero_features_in_matrix(X)
-    df = pd.DataFrame(features)
-    feature_filename = mkfilename("zero_features", args.config, args.uncore)
-    df.to_csv(os.path.join(args.data_directory, feature_filename), index=False, header=['EVENT_NAME'])
-
-    events = sorted(set([ feature.split(".", 1)[1] for feature in features ]))
-    event_filename = mkfilename("zero_events", args.config, args.uncore)
-    df = pd.DataFrame(events)
-    df.to_csv(os.path.join(args.data_directory, event_filename), index=False, header=['EVENT_NAME'])
+    print(all_zero_features(args.data_directory, args.config, args.uncore))
