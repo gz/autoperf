57aa98e705d1accd1444de61d58a3acf1c3e4a27
Gerd Zellweger <mail@gerdzellweger.com>
2016-11-04 14:20:08 +0100

    Update svm machine aware script.
    Signed-off-by: Gerd Zellweger <mail@gerdzellweger.com>

--- analyze/classify/svm_machine_aware.py
+++ analyze/classify/svm_machine_aware.py
@@ -11,19 +11,19 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt, font_manager
 
-from .runtimes import get_runtime_dataframe, get_runtime_pivot_tables
-from util import *
-
 from sklearn import svm
 from sklearn import metrics
 from sklearn import preprocessing
 
-from .svm import row_training_and_test_set, get_svm_metrics
-from .svm_topk import get_selected_events
-
-AUTOPERF_PATH = os.path.join(sys.path[0], "..", "..", "target", "release", "autoperf")
+sys.path.insert(1, os.path.join(os.path.realpath(os.path.split(__file__)[0]), '..', ".."))
+from analyze.classify.svm import row_training_and_test_set, get_svm_metrics, make_svm_result_filename
+from analyze.classify.svm_topk import get_selected_events, make_ranking_filename
+from analyze.classify.runtimes import get_runtime_dataframe, get_runtime_pivot_tables
+from analyze.classify import get_argument_parser
+from analyze.util import *
 
 def mkgroup(cfs_ranking_file):
+    AUTOPERF_PATH = os.path.join(sys.path[0], "..", "..", "target", "release", "autoperf")
     ret = subprocess.check_output([AUTOPERF_PATH, "mkgroup", "--input", cfs_ranking_file])
     lines = ret.split(os.linesep)
     assert lines[-1] == ''
@@ -48,8 +48,7 @@ if __name__ == '__main__':
 
         for test in tests:
             if not args.cfs:
-                cfs_default_file = os.path.join(args.data_directory, "topk_svm_{}_{}.csv"
-                    .format('_'.join(test), '_'.join(args.config)))
+                cfs_default_file = os.path.join(args.data_directory, make_ranking_filename(test, args))
                 if not os.path.exists(cfs_default_file):
                     print(("Can't process {} because we didn't find the CFS file {}".format(A, cfs_default_file)))
                     sys.exit(1)
@@ -58,7 +57,7 @@ if __name__ == '__main__':
             else:
                 event_list = mkgroup(args.cfs)
 
-            X_all, Y, X_test_all, Y_test = row_training_and_test_set(args, test)
+            X_all, Y, X_test_all, Y_test = row_training_and_test_set(args.data_directory, args.config, test, uncore=args.uncore, features=args.features, cutoff=args.cutoff, include_alone=args.include_alone, drop_zero=args.dropzero)
 
             X = pd.DataFrame()
             X_test = pd.DataFrame()
@@ -77,5 +76,5 @@ if __name__ == '__main__':
             row = get_svm_metrics(args, test, Y, Y_test, Y_pred)
             results_table = results_table.append(row, ignore_index=True)
 
-        filename = make_result_filename("svm_results_machine_aware", args, kconfig)
+        filename = make_svm_result_filename("svm_results_machine_aware", args, kconfig)
         results_table.to_csv(filename + ".csv", index=False)
