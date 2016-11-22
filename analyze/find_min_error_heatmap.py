8902e0a36faa519ad035461bde19c5c57f5212a1
Gerd Zellweger <mail@gerdzellweger.com>
2016-11-22 18:00:45 +0100

    Added script to find minimum error in heatmaps.
    Signed-off-by: Gerd Zellweger <mail@gerdzellweger.com>

---
+++ analyze/find_min_error_heatmap.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+import pandas as pd
+
+if __name__ == '__main__':
+    print("Finding min error heatmap")
+    heatmaps = []
+    for root, dirs, files in os.walk(sys.argv[1]):
+        for name in files:
+            p = os.path.join(root, name)
+            if p.endswith(".csv"):
+                df = pd.read_csv(p)
+                heatmaps.append( (len(df[df.Accuracy > 0.70]), p) )
+
+    for score, path in sorted(heatmaps):
+        print (score, path)
