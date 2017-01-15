e102fd1f2a81d751648bc92e102be171819f7e6c
Gerd Zellweger <mail@gerdzellweger.com>
2017-01-15 14:27:23 +0100

    Add more scripts
    Signed-off-by: Gerd Zellweger <mail@gerdzellweger.com>

---
+++ analyze/classify/svm_smtscaling.py
@@ -0,0 +1,267 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import time
+import argparse
+import math
+import logging
+
+import pandas as pd
+import numpy as np
+
+from sklearn import svm
+from sklearn import metrics
+from sklearn import preprocessing
+from sklearn import neural_network
+from sklearn import tree
+from sklearn import neighbors
+from sklearn import ensemble
+from sklearn import linear_model
+
+sys.path.insert(1, os.path.join(os.path.realpath(os.path.split(__file__)[0]), '..', ".."))
+from analyze.classify import get_argument_parser
+from analyze.classify.runtimes import get_runtime_dataframe, get_runtime_pivot_tables
+from analyze.classify.generate_matrix import matrix_file_name
+from analyze.util import *
+
+
+CLASSIFIERS = {
+    'linear': svm.SVC(kernel='linear'),
+    'linearbalanced': svm.SVC(kernel='linear', class_weight='balanced'),
+    'poly1': svm.SVC(kernel='poly', degree=1),
+    'poly2': svm.SVC(kernel='poly', degree=2),
+    'poly3': svm.SVC(kernel='poly', degree=3),
+    'poly1balancedC1.00': svm.SVC(kernel='poly', degree=1, class_weight='balanced', C=1.0),
+    'poly1balancedC1.5': svm.SVC(kernel='poly', degree=1, class_weight='balanced', C=1.5),
+    'poly1balancedC0.01': svm.SVC(kernel='poly', degree=1, class_weight='balanced', C=0.01),
+    'poly1balancedC0.05': svm.SVC(kernel='poly', degree=1, class_weight='balanced', C=0.05),
+    'poly1balancedC0.04': svm.SVC(kernel='poly', degree=1, class_weight='balanced', C=0.04),
+    'poly1balancedC0.06': svm.SVC(kernel='poly', degree=1, class_weight='balanced', C=0.06),
+    'poly1balancedC0.07': svm.SVC(kernel='poly', degree=1, class_weight='balanced', C=0.07),
+    'poly1balancedC0.08': svm.SVC(kernel='poly', degree=1, class_weight='balanced', C=0.08),
+    'poly1balancedC0.10': svm.SVC(kernel='poly', degree=1, class_weight='balanced', C=0.1),
+    'poly1balancedC0.20': svm.SVC(kernel='poly', degree=1, class_weight='balanced', C=0.2),
+    'poly1balancedC0.30': svm.SVC(kernel='poly', degree=1, class_weight='balanced', C=0.3),
+    'poly1balancedC0.50': svm.SVC(kernel='poly', degree=1, class_weight='balanced', C=0.5),
+    'poly2balancedC0.10': svm.SVC(kernel='poly', degree=2, class_weight='balanced', C=0.1),
+    'poly2balancedC0.50': svm.SVC(kernel='poly', degree=2, class_weight='balanced', C=0.5),
+    'poly2balancedC1.00': svm.SVC(kernel='poly', degree=2, class_weight='balanced', C=1.00),
+    'poly2balancedC1.2': svm.SVC(kernel='poly', degree=2, class_weight='balanced', C=1.2),
+    'poly2balancedC1.50': svm.SVC(kernel='poly', degree=2, class_weight='balanced', C=1.50),
+    'poly2balancedC1.1': svm.SVC(kernel='poly', degree=2, class_weight='balanced', C=1.1),
+    'poly2balancedC1.2': svm.SVC(kernel='poly', degree=2, class_weight='balanced', C=1.2),
+    'poly2balancedC1.3': svm.SVC(kernel='poly', degree=2, class_weight='balanced', C=1.3),
+    'poly2balancedC1.6': svm.SVC(kernel='poly', degree=2, class_weight='balanced', C=1.6),
+    'poly2balancedC1.8': svm.SVC(kernel='poly', degree=2, class_weight='balanced', C=1.8),
+    'poly2balancedC1.90': svm.SVC(kernel='poly', degree=2, class_weight='balanced', C=1.90),
+    'poly2balancedC2.00': svm.SVC(kernel='poly', degree=2, class_weight='balanced', C=2.00),
+    'poly2balancedC2.10': svm.SVC(kernel='poly', degree=2, class_weight='balanced', C=2.10),
+    'poly2balancedC2.2': svm.SVC(kernel='poly', degree=2, class_weight='balanced', C=2.2),
+    'poly2balancedC2.3': svm.SVC(kernel='poly', degree=2, class_weight='balanced', C=2.3),
+    'poly2balancedC2.4': svm.SVC(kernel='poly', degree=2, class_weight='balanced', C=2.4),
+    'poly2balancedC2.50': svm.SVC(kernel='poly', degree=2, class_weight='balanced', C=2.5),
+    'poly2balancedC0.10': svm.SVC(kernel='poly', degree=2, class_weight='balanced', C=0.1),
+    'poly2balancedC0.01': svm.SVC(kernel='poly', degree=2, class_weight='balanced', C=0.01),
+    'poly2balancedC3.5': svm.SVC(kernel='poly', degree=2, class_weight='balanced', C=3.5),
+    'poly2balancedC4.5': svm.SVC(kernel='poly', degree=2, class_weight='balanced', C=4.5),
+    'poly2balancedC5.5': svm.SVC(kernel='poly', degree=2, class_weight='balanced', C=5.5),
+    'poly2balancedC10': svm.SVC(kernel='poly', degree=2, class_weight='balanced', C=10),
+    'poly2balancedC20': svm.SVC(kernel='poly', degree=2, class_weight='balanced', C=20),
+    'poly2balancedC100': svm.SVC(kernel='poly', degree=2, class_weight='balanced', C=100),
+    'poly3balanced': svm.SVC(kernel='poly', degree=3, class_weight='balanced'),
+    'poly3balancedC0.51': svm.SVC(kernel='poly', degree=3, class_weight='balanced', C=0.51),
+    'rbf1': svm.SVC(kernel='rbf', degree=1),
+    'rbf1balanced': svm.SVC(kernel='rbf', degree=1, class_weight='balanced'),
+    'rbf2balancedC2': svm.SVC(kernel='rbf', degree=2, class_weight='balanced', C=2),
+    'rbf2balanced': svm.SVC(kernel='rbf', degree=2, class_weight='balanced'),
+    'rbf1balanced': svm.SVC(kernel='rbf', degree=2, class_weight='balanced'),
+    'neural': neural_network.MLPClassifier(),
+    'neuralsgd': neural_network.MLPClassifier(solver='sgd'),
+    'neuraladaptivelogistic': neural_network.MLPClassifier(activation='logistic', learning_rate='adaptive'),
+    'passiveaggr': linear_model.PassiveAggressiveClassifier(),
+    'randomforest10': ensemble.RandomForestClassifier(n_estimators=10),
+    'randomforest20': ensemble.RandomForestClassifier(n_estimators=20),
+    'randomforest30': ensemble.RandomForestClassifier(n_estimators=30),
+    'decision': tree.DecisionTreeClassifier(),
+    'decision5': tree.DecisionTreeClassifier(max_features=5),
+    'decision10': tree.DecisionTreeClassifier(max_features=10),
+    'decision15': tree.DecisionTreeClassifier(max_features=15),
+    'decision20': tree.DecisionTreeClassifier(max_features=20),
+    'decision120': tree.DecisionTreeClassifier(max_features=120),
+    'kneighbors': neighbors.KNeighborsClassifier(),
+    'kneighborsdistance': neighbors.KNeighborsClassifier(weights='distance'),
+    'adaboost': ensemble.AdaBoostClassifier()
+}
+
+C_RANGE = np.arange(0.10, 10, 0.10)
+CLASSIFIERS.update(dict(('poly1balancedC{:.2f}'.format(C), svm.SVC(kernel='poly', class_weight='balanced', degree=1, C=C)) for C in C_RANGE))
+CLASSIFIERS.update(dict(('poly2balancedC{:.2f}'.format(C), svm.SVC(kernel='poly', class_weight='balanced', degree=2, C=C)) for C in C_RANGE))
+CLASSIFIERS.update(dict(('poly1C{:.2f}'.format(C), svm.SVC(kernel='poly', degree=1, C=C)) for C in C_RANGE))
+CLASSIFIERS.update(dict(('poly2C{:.2f}'.format(C), svm.SVC(kernel='poly', degree=2, C=C)) for C in C_RANGE))
+CLASSIFIERS.update(dict(('poly3C{:.2f}'.format(C), svm.SVC(kernel='poly', degree=3, C=C)) for C in C_RANGE))
+CLASSIFIERS.update(dict(('poly3balancedC{:.2f}'.format(C), svm.SVC(kernel='poly', class_weight='balanced', degree=3, C=C)) for C in C_RANGE))
+CLASSIFIERS.update(dict(('rbf3{:.2f}'.format(C), svm.SVC(kernel='rbf', degree=3, C=C)) for C in C_RANGE))
+CLASSIFIERS.update(dict(('rbf3balanced{:.2f}'.format(C), svm.SVC(kernel='rbf', class_weight='balanced', degree=3, C=C)) for C in C_RANGE))
+
+
+SMT_SPEEDUP = {
+    'PR700': 1.0,
+    'AA700': 1.33,
+    'HD1400': 1.5,
+    'TC1400': 1.53,
+    'NBODY': 1.05,
+    'BSCHOL': 1.74,
+    'CNEAL': 1.48,
+    'FERR': 1.33,
+    'SCLUS': 1.07,
+    'SWAPT': 1.07
+}
+
+def drop_zero_events(args, df):
+    from analyze.classify.find_all_zero import zero_features
+    to_drop = zero_features(args, overwrite=False)
+    df.drop(to_drop['EVENT_NAME'], axis=1, inplace=True)
+
+def row_training_and_test_set(args, tests):
+    assert(len(tests) == 1)
+    X = []
+    Y = []
+    Y_weights = []
+
+    X_test = []
+    Y_test = []
+
+    runtimes = get_runtime_dataframe(args.data_directory)
+    for config, table in get_runtime_pivot_tables(runtimes):
+        if config in args.config:
+            for (A, values) in table.iterrows():
+                for (i, normalized_runtime) in enumerate(values):
+                    B = table.columns[i]
+
+                    classification = True if SMT_SPEEDUP[A] > args.cutoff else False
+                    if B != "Alone":
+                        logging.debug("Skipping the samples that are not alone")
+                        continue
+
+                    results_path = os.path.join(args.data_directory, config, "{}".format(A))
+                    matrix_file_path = os.path.join(results_path, matrix_file_name(args.core, args.uncore, args.features))
+
+                    if os.path.exists(os.path.join(results_path, 'completed')):
+                        if not os.path.exists(matrix_file_path):
+                            logging.error("No matrix file ({}) found, run the generate_matrix.py script first!".format(matrix_file_path))
+                            sys.exit(1)
+
+                        df = pd.read_csv(matrix_file_path, index_col=False)
+                        if args.dropzero:
+                            #logging.debug("Dropping zero")
+                            drop_zero_events(args, df)
+                        if A in tests:
+                            logging.debug("Adding {} vs {} to test set class={} file={}".format(A, B, classification, matrix_file_path))
+                            Y_test.append(pd.Series([classification for _ in range(0, df.shape[0])]))
+                            X_test.append(df)
+                        elif B in tests:
+                            logging.debug("Discarding {} vs {} class={} file={}".format(A, B, classification, matrix_file_path))
+                            pass
+                        else:
+                            logging.debug("Adding {} vs {} to training set class={} file={}".format(A, B, classification, matrix_file_path))
+                            Y.append(pd.Series([classification for _ in range(0, df.shape[0])]))
+                            Y_weights.append(pd.Series([1 for _ in range(0, df.shape[0])]))
+                            X.append(df)
+                    else:
+                        print(("Exclude unfinished directory {}".format(results_path)))
+
+    return (pd.concat(X), pd.concat(Y), pd.concat(Y_weights),
+            pd.concat(X_test) if len(X_test) > 0  else None,
+            pd.concat(Y_test) if len(Y_test) > 0 else None)
+
+def get_svm_metrics(args, test, Y, Y_test, Y_pred):
+    row = {}
+    row['Training Configs'] = ' '.join(args.config)
+    row['Tested Application'] = ' '.join(test)
+
+    row['Samples Training Total'] = "{}".format(len(Y))
+    row['Samples Test Total'] = "{}".format(len(Y_test))
+
+    row['Samples Training 0'] = len(Y[Y == 0])
+    row['Samples Training 1'] = len(Y[Y == 1])
+    row['Samples Test 0'] = len(Y_test[Y_test == 0])
+    row['Samples Test 1'] = len(Y_test[Y_test == 1])
+
+    row['Accuracy'] = "%.2f" % metrics.accuracy_score(Y_test, Y_pred)
+    row['Error'] = "%.2f" % (1.0 - metrics.accuracy_score(Y_test, Y_pred))
+    row['Precision'] = "%.2f" % metrics.precision_score(Y_test, Y_pred)
+    row['Recall'] = "%.2f" % metrics.recall_score(Y_test, Y_pred)
+    row['F1 score'] = "%.2f" % metrics.f1_score(Y_test, Y_pred)
+
+    return row
+
+def make_suffixes(args):
+    alone_suffix = "alone" if args.include_alone else "paironly"
+    dropzero_suffix = "dropzero" if args.dropzero else "inczero"
+    cutoff_suffix = "{}".format(math.ceil(args.cutoff*100))
+    return (alone_suffix, dropzero_suffix, cutoff_suffix)
+
+def make_svm_result_filename(prefix, args, kconfig):
+    alone_suffix, dropzero_suffix, cutoff_suffix = make_suffixes(args)
+
+    filename = prefix + "_training_{}_core_{}_uncore_{}_features_{}_{}_{}_{}_{}" \
+               .format("_".join(sorted(args.config)), args.core, args.uncore, "_".join(sorted(args.features)),
+                       kconfig, alone_suffix, dropzero_suffix, cutoff_suffix)
+    return filename
+
+def make_weka_results_filename(prefix, args):
+    alone_suffix, dropzero_suffix, cutoff_suffix = make_suffixes(args)
+    filename = '{}_training_{}_core_{}_uncore_{}_features_{}_{}_{}_{}.csv'
+    return filename.format(prefix, '_'.join(sorted(args.config)), \
+                           args.core, args.uncore, "_".join(sorted(args.features)), \
+                           alone_suffix, dropzero_suffix, cutoff_suffix)
+
+if __name__ == '__main__':
+    parser = get_argument_parser('Get the SVM parameters for a row in the heatmap.')
+    parser.add_argument('--weka', dest='weka', action='store_true', default=False, help='Save files for Weka')
+    parser.add_argument('--tests', dest='tests', nargs='+', type=str, help="List or programs to include for the test set.")
+    parser.add_argument('--all', dest='all', action='store_true', default=False, help="Use whole data set as training set.")
+    args = parser.parse_args()
+
+    if not args.tests:
+        runtimes = get_runtime_dataframe(args.data_directory)
+        tests = [[x] for x in sorted(runtimes['A'].unique())] # None here means we save the whole matrix as X (no training set)
+    elif args.all:
+        tests = [[None]]
+    else:
+        tests = [args.tests] # Pass the tests as a single set
+
+    if not args.weka:
+
+        if args.kernel:
+            kernels = [ (args.kernel, CLASSIFIERS[args.kernel]) ]
+        else:
+            kernels = list(CLASSIFIERS.items())
+
+        if args.paper:
+            output_directory = os.getcwd()
+        else:
+            output_directory = os.path.join(args.data_directory, "results_svm")
+            os.makedirs(output_directory, exist_ok=True)
+
+        for kconfig, clf in kernels:
+            print("Trying kernel", kconfig)
+            results_table = pd.DataFrame()
+
+            for test in tests:
+                X, Y, Y_weights, X_test, Y_test = row_training_and_test_set(args, test)
+                min_max_scaler = preprocessing.MinMaxScaler()
+                X_scaled = min_max_scaler.fit_transform(X)
+
+                if test != [None]:
+                    X_test_scaled = min_max_scaler.transform(X_test)
+                    clf.fit(X_scaled, Y)
+                    Y_pred = clf.predict(X_test_scaled)
+
+                    row = get_svm_metrics(args, test, Y, Y_test, Y_pred)
+                    results_table = results_table.append(row, ignore_index=True)
+                    print(results_table)
+
+            filename = make_svm_result_filename("svm_scalesmt_results", args, kconfig)
+            results_table.to_csv(os.path.join(output_directory, filename + ".csv"), index=False)
--- src/scale.rs
+++ src/scale.rs
@@ -17,6 +17,162 @@ use toml;
 
 use super::util::*;
 
-pub fn scale(manifest_folder: &Path, dryrun: bool) {
-    panic!("NYI");
+pub fn scale(manifest_folder: &Path, dryrun: bool, start: usize, stepping: usize) {
+    let canonical_manifest_path = fs::canonicalize(&manifest_folder)
+        .expect("canonicalize manifest path does not work");
+
+    let mut out_dir = canonical_manifest_path.to_path_buf();
+    let hostname = get_hostname().unwrap_or(String::from("unknown"));
+    out_dir.push(hostname);
+    mkdir(&out_dir);
+
+    let mt = MachineTopology::new();
+
+    let mut manifest: PathBuf = canonical_manifest_path.to_path_buf();
+    manifest.push("manifest.toml");
+    let mut file = File::open(manifest.as_path()).expect("manifest.toml file does not exist?");
+    let mut manifest_string = String::new();
+    let _ = file.read_to_string(&mut manifest_string).unwrap();
+    let mut parser = toml::Parser::new(manifest_string.as_str());
+    let doc = match parser.parse() {
+        Some(doc) => doc,
+        None => {
+            error!("Can't parse the manifest file:\n{:?}", parser.errors);
+            process::exit(1);
+        }
+    };
+    let experiment: &toml::Table = doc["experiment"]
+        .as_table()
+        .expect("Error in manifest.toml: 'experiment' should be a table.");
+    let configuration: &[toml::Value] = experiment["configurations"]
+        .as_slice()
+        .expect("Error in manifest.toml: 'configuration' attribute should be a list.");
+    let configs: Vec<String> = configuration.iter()
+        .map(|s| s.as_str().expect("configuration elements should be strings").to_string())
+        .collect();
+    let run_alone: bool = experiment.get("alone")
+        .map_or(true, |v| v.as_bool().expect("'alone' should be boolean"));
+    let profile_only: Option<Vec<String>> = experiment.get("profile_only_a")
+        .map(|progs| {
+            progs.as_slice()
+                .expect("Error in manifest.toml: 'profile_only_a' should be a list.")
+                .into_iter()
+                .map(|p| {
+                    p.as_str()
+                        .expect("profile_only_a elements should name programs (strings)")
+                        .to_string()
+                })
+                .collect()
+        });
+    let profile_only_b: Option<Vec<String>> = experiment.get("profile_only_b")
+        .map(|progs| {
+            progs.as_slice()
+                .expect("Error in manifest.toml: 'profile_only_b' should be a list.")
+                .into_iter()
+                .map(|p| {
+                    p.as_str()
+                        .expect("profile_only_b elements should name programs (strings)")
+                        .to_string()
+                })
+                .collect()
+        });
+
+
+    let mut programs: Vec<Program> = Vec::with_capacity(2);
+    for (key, value) in &doc {
+        if key.starts_with("program") {
+            let program_desc: &toml::Table =
+                doc[key].as_table().expect("Error in manifest.toml: 'program' should be a table.");
+            programs.push(Program::from_toml(&canonical_manifest_path, program_desc, run_alone));
+        }
+    }
+
+    let mut deployments: Vec<Deployment> = Vec::with_capacity(4);
+    for config in configs {
+        match config.as_str() {
+            "L1-SMT" => {
+                deployments.push(Deployment::split_interleaved("L1-SMT",
+                                                               mt.same_l1(),
+                                                               mt.l1_size().unwrap_or(0)))
+            }
+            "L3-SMT" => {
+                deployments.push(Deployment::split_interleaved("L3-SMT",
+                                                               mt.same_l3(),
+                                                               mt.l3_size().unwrap_or(0)))
+            }
+            "L3-SMT-cores" => {
+                deployments.push(Deployment::split_smt_aware("L3-SMT-cores",
+                                                             mt.same_l3(),
+                                                             mt.l3_size().unwrap_or(0)))
+            }
+            "L3-cores" => {
+                deployments.push(Deployment::split_smt_aware("L3-cores",
+                                                             mt.same_l3_cores(),
+                                                             mt.l3_size().unwrap_or(0)))
+            }
+            "Full-L3" => {
+                deployments.push(Deployment::split_l3_aware("Full-L3",
+                                                            mt.whole_machine_cores(),
+                                                            mt.l3_size().unwrap_or(0)))
+            }
+            "Full-SMT-L3" => {
+                deployments.push(Deployment::split_l3_aware("Full-SMT-L3",
+                                                            mt.whole_machine(),
+                                                            mt.l3_size().unwrap_or(0)))
+            }
+            "Full-cores" => {
+                deployments.push(Deployment::split_interleaved("Full-cores",
+                                                               mt.whole_machine_cores(),
+                                                               mt.l3_size().unwrap_or(0)))
+            }
+            "Full-SMT-cores" => {
+                deployments.push(Deployment::split_smt_aware("Full-SMT-cores",
+                                                             mt.whole_machine(),
+                                                             mt.l3_size().unwrap_or(0)))
+            }
+
+            _ => error!("Ignored unknown deployment config '{}'.", config),
+        };
+    }
+
+    // Add all possible pairs:
+    let mut pairs: Vec<(&Program, Option<&Program>)> = Vec::new();
+    for p in programs.iter() {
+        pairs.push((p, None));
+    }
+    for (a, b) in iproduct!(programs.iter(), programs.iter()) {
+        pairs.push((a, Some(b)));
+    }
+
+    // Filter out the pairs we do not want to execute:
+    let mut runs: Vec<Run> = Vec::new();
+    for (a, b) in pairs.into_iter() {
+        let profile_a = profile_only.as_ref().map_or(true, |ps| ps.contains(&a.name));
+        let profile_b = !b.is_none() &&
+                        profile_only_b.as_ref()
+            .map_or(profile_a, |ps| ps.contains(&b.unwrap().name));
+        if !profile_a && !profile_b {
+            continue;
+        }
+
+        for d in deployments.iter() {
+            if b.is_none() && (!run_alone || !a.alone) {
+                continue;
+            }
+            runs.push(Run::new(&canonical_manifest_path, out_dir.as_path(), a, b, d));
+        }
+    }
+
+    // Finally, profile the runs we are supposed to execute based on the command line args
+    let mut i = 0;
+    for run in runs.iter_mut().skip(start).step(stepping) {
+        if !dryrun {
+            run.profile();
+        } else {
+            println!("{}", run);
+        }
+        i += 1;
+    }
+
+    println!("{} runs completed.", i);
 }
